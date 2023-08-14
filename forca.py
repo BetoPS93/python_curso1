2ba9d5b32f3c5a36a38747660b7264d60354bdc7
Betops93 <adalbertops93@live.com>
2023-08-14 19:41:40 -0300

    aula07Curso02

--- forca.py
+++ forca.py
@@ -1,25 +1,13 @@
 from sympy import IndexedBase, LessThan, false, li, true
 import random
 
+
 def jogar():
-    print("**********************************")
-    print("Bem vindo ao jogo de Forca!")
-    print("**********************************")
+    imprime_msg_abrt()
 
+    palavra_secreta = carrega_palavra_secreta()
 
-    arquivo = open("palavras.txt", "r")
-    palavras = []
-
-    for linha in arquivo:
-        linha = linha.strip()
-        palavras.append(linha)
-
-    arquivo.close()
-
-    numero = random.randrange(0,len(palavras))
-
-    palavra_secreta = palavras[numero].upper()
-    letras_acertadas = ["_" for letra in palavra_secreta]
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
 
     enforcou = False
     acertou = False
@@ -28,15 +16,11 @@ def jogar():
     print(letras_acertadas)
 
     while not enforcou and not acertou:
-        chute = input("Qual letra? ")
-        chute = chute.strip().upper()
+        
+        chute = pede_chute()
 
         if chute in palavra_secreta:
-            index = 0
-            for letra in palavra_secreta:
-                if chute == letra:
-                    letras_acertadas[index] = letra
-                index += 1
+            marca_chute_correto(chute, letras_acertadas, palavra_secreta)
         else:
             erros -= 1
             print(f"Ops, você errou! Faltam {erros} tentativas")
@@ -46,11 +30,59 @@ def jogar():
         print(letras_acertadas)
 
     if acertou:
-        print("Você ganhou!!")
+        imprime_msg_vencedor()
     else:
-        print("Você perdeu...")
+        imprime_mensagem_perdedor()
 
-    print("******Fim de jogo.********")
+
+def imprime_msg_abrt():
+    print("**********************************")
+    print("Bem vindo ao jogo de Forca!")
+    print("**********************************")
+
+
+def carrega_palavra_secreta():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+
+    palavra_secreta = palavras[numero].upper()
+
+    return palavra_secreta
+
+
+def inicializa_letras_acertadas(palavra_secreta):
+    return ["_" for letra in palavra_secreta]
+
+
+def pede_chute():
+    chute = input("Qual letra? ")
+    chute = chute.strip().upper()
+    return chute
+
+
+def marca_chute_correto(chute, letras_acertadas, palavra_secreta):
+    index = 0
+    for letra in palavra_secreta:
+        if chute == letra:
+            letras_acertadas[index] = letra
+        index += 1
+    return
+
+
+def imprime_msg_vencedor():
+    print("Você ganhou!!")
+
+
+def imprime_mensagem_perdedor():
+    print("Você perdeu...")
 
 
 if __name__ == "__main__":
